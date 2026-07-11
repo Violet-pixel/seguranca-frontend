01d1ab47626023a6fa69cc4b6aa870f308823f5b
CLARA ARAGAO PORTES <clara.portes@aluno.senac.rio>
2026-07-10 21:25:28 -0300

    versao 12

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router'
+import { EpiList } from './epi/epi-list/epi-list';
+import { EpiDetalhe } from './epi/epi-detalhe/epi-detalhe';
+ 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EpiList, EpiDetalhe, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('seguranca-frontend');
+ titulo = "Sistema de controle de EPI;";
+ subtitulo = "Gestão de segurança do trabalho";
+ descricao = "Aplicação para controle, monitoramento e conformidade dos equipamentos de proteção individual";
+ 
 }
---
+++ src/app/epi/epi-detalhe/epi-detalhe.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit  } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-epi-detalhe',
+  imports: [],
+  templateUrl: './epi-detalhe.html',
+  styleUrl: './epi-detalhe.scss',
+})
+export class EpiDetalhe implements OnInit {​
+
+  id: number = 0; // Variável que vai armazenar o ID vindo da URL​
+
+  // O Angular injeta o serviço ActivatedRoute automaticamente​
+
+  // Ele nos dá acesso aos parâmetros da URL (query string nesse caso)​
+
+constructor(private route: ActivatedRoute) { }​
+
+ngOnInit() {  // Método executado quando o componente é carregado​
+
+  // queryParams é um observable (fluxo de dados da URL)​
+
+  this.route.queryParams.subscribe(params => {    // subscribe = "ficar escutando mudanças na URL"​
+    // Pegamos o valor do parâmetro 'id' vindo da URL​
+
+    // Exemplo de URL: /epi/detalhe?id=10​
+
+    this.id = params['id'];​
+
+    // Agora o valor pode ser usado no HTML:​
+
+    // Exemplo: {{ id }}​
+
+  });​
+
+}​
+
+}​
